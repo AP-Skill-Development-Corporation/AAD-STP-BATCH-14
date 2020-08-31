e79c4c8e510571ef49a6f7430c1baf5eac51313e
Muneiahtellakula <muneiahtellakula@gmail.com>
2020-08-31 17:05:55 +0530

    Day -7 UI Components Project is added

---
+++ UiComonentsDemo/app/src/main/java/com/muneiah/uicomonentsdemo/MainActivity.java
@@ -0,0 +1,45 @@
+package com.muneiah.uicomonentsdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.RatingBar;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+SeekBar mSeekbar;
+RatingBar mRatingBar;
+TextView rr,sr;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mRatingBar=findViewById(R.id.rb);
+        mSeekbar=findViewById(R.id.sb);
+        rr=findViewById(R.id.result_rb);
+        sr=findViewById(R.id.sb_result);
+        mRatingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                rr.setText("Rating is: "+rating);
+            }
+        });
+        mSeekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                sr.setText("progress is :"+progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+}
