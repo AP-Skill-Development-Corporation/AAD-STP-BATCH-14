d3aa209db9f5b6e2dd711960f49e4aed0b18f2fa
devianusha <neelapudevianusha@gmail.com>
2020-09-03 17:50:33 +0530

    navigationcomponent

---
+++ NavigationComponents/app/src/main/java/com/example/navigationcomponents/FirstFragment.java
@@ -0,0 +1,51 @@
+package com.example.navigationcomponents;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+public class FirstFragment extends Fragment {
+
+
+
+    public FirstFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view=inflater.inflate(R.layout.fragment_first,container,false);
+        Button red=view.findViewById(R.id.red);
+        red.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.action_firstFragment_to_redFragment);
+            }
+        });
+        Button blue=view.findViewById(R.id.blue);
+        blue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.action_firstFragment_to_blueFragment);
+            }
+        });
+        Button green =view.findViewById(R.id.Green);
+        green.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.action_firstFragment_to_greenFragment2);
+            }
+        });
+        return view;
+    }
+}
