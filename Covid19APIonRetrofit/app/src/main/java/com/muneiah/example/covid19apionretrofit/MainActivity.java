6e29d7f6402f131ee6d690fb4b44172d060cb8b8
Muneiahtellakula <muneiahtellakula@gmail.com>
2020-09-06 18:52:41 +0530

    Day 13 Covid19 API explaring using Retrofit

--- Covid19APIonRetrofit/app/src/main/java/com/muneiah/example/covid19apionretrofit/MainActivity.java
+++ Covid19APIonRetrofit/app/src/main/java/com/muneiah/example/covid19apionretrofit/MainActivity.java
@@ -9,6 +9,14 @@ import android.widget.Toast;
 
 import com.muneiah.example.covid19apionretrofit.databinding.ActivityMainBinding;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -24,7 +32,29 @@ ActivityMainBinding binding;
         c.enqueue(new Callback<String>() {
             @Override
             public void onResponse(Call<String> call, Response<String> response) {
-                Toast.makeText(MainActivity.this, ""+response.body(), Toast.LENGTH_SHORT).show();
+               // Toast.makeText(MainActivity.this, ""+response.body(), Toast.LENGTH_SHORT).show();
+                try {
+                    JSONArray rootArray=new JSONArray(response.body());
+                    for (int i=0;i<rootArray.length();i++)
+                    {
+                        JSONObject rootObj = rootArray.getJSONObject(i);
+                        String countryResult = rootObj.getString("Country");
+                        String confiredResult = rootObj.getString("Confirmed");
+                        String resultDeaths = rootObj.getString("Deaths");
+                        String resultRecovered = rootObj.getString("Recovered");
+                        String resultActive = rootObj.getString("Active");
+                        String resultDate = rootObj.getString("Date");
+                        binding.activeTv.setText("Active " + resultActive);
+                        binding.confiredTv.setText("Confirmed " + confiredResult);
+                        binding.countryTv.setText("Country " + countryResult);
+                        binding.deathsTv.setText("Deaths " + resultDeaths);
+                        binding.recoveredTv.setText("Confirmed " + resultRecovered);
+                        binding.dateTv.setText("Date " + properDateFormat(resultDate));
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
             }
 
             @Override
@@ -32,7 +62,26 @@ ActivityMainBinding binding;
                 Toast.makeText(MainActivity.this, "something went wrong with fetching data..", Toast.LENGTH_SHORT).show();
             }
         });
-
+/*how many of you get the output*/
 
     }
+
+    private String properDateFormat(String resultDate) {
+        String inputFormat="yy-mm-dd";
+        String outputformat="dd-mm-yy";
+        SimpleDateFormat inputForm=new SimpleDateFormat(inputFormat);
+        SimpleDateFormat outputForm=new SimpleDateFormat(outputformat);
+       String s=null;
+        Date d=null;
+        try {
+            d=inputForm.parse(resultDate);
+            s=outputForm.format(d);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+
+        return s;
+    }
+
 }
